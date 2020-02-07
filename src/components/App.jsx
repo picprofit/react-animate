09874aaf851b7c488816c381af52971370cd72c0
Konstantin <picprofit@gmail.com>
2020-02-07 15:22:25 +0300

    initial

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        Hi there!
+      </header>
+    </>
+  );
+}
+
+export default App;
