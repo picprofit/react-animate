d1da4c585851f5bbf0fa4176bcff27abd0d6f710
Konstantin <picprofit@gmail.com>
2020-02-10 14:29:43 +0300

    add: SD animations

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import parse from 'html-react-parser';
+import { motion } from 'framer-motion';
 
-const StyledProject = styled.div`
+const StyledProject = styled(motion.div)`
   background: #fff;
   color: #58595b;
   border: 1px solid #eee;
@@ -18,27 +19,79 @@ const StyledProject = styled.div`
   align-items: center;
   justify-content: center;
   overflow: hidden;
+  cursor: pointer;
   &:hover {
     box-shadow: 10px 10px 15px -10px rgba(88, 89, 91, 0.45);
   }
+`;
+
+const pixelsToMove = 100;
+
+const cssVisible = `
+  opacity: 1;
+  top: 0;
+  height: auto;
+  overflow: auto;
+  margin: auto;
+  padding: auto;
+`;
+const cssHidden = `
+  opacity: 0;
+  height: 0;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
 `;
 
 const StyledTitle = styled.h3`
   color: #000;
   font-size: 22px;
   font-weight: 500;
+  position: relative;
+  transition: all 1s;
+  ${props =>
+    props.ishover
+      ? `
+  top: -${pixelsToMove}px;
+  ${cssHidden}
+  `
+      : `
+  ${cssVisible}
+  `}
 `;
 
 const StyledDescription = styled.div`
   color: #000;
   font-size: 18px;
+  position: relative;
+  transition: all 0.7s;
+  ${props =>
+    props.ishover
+      ? `
+  ${cssVisible}
+  `
+      : `
+  top: ${pixelsToMove}px;
+  ${cssHidden}
+  `}
 `;
 
 const Project = ({ title, description }) => {
+  const [isHover, setHover] = useState(false);
+
   return (
-    <StyledProject>
-      <StyledTitle>{title}</StyledTitle>
-      <StyledDescription>{parse(description)}</StyledDescription>
+    <StyledProject
+      onHoverStart={() => {
+        setHover(true);
+      }}
+      onHoverEnd={() => {
+        setHover(false);
+      }}
+    >
+      <StyledTitle ishover={isHover ? 1 : 0}>{title}</StyledTitle>
+      <StyledDescription ishover={isHover ? 1 : 0}>
+        {parse(description)}
+      </StyledDescription>
     </StyledProject>
   );
 };
