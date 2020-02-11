a20845909544e6c8adb2c62ccf5c6c4d85e52dc9
Konstantin <picprofit@gmail.com>
2020-02-11 11:49:52 +0300

    add: See my CV button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 import Services from './Services';
+
+const StyledButton = styled(NavLink)`
+  background: transparent;
+  border: 0;
+  text-decoration: none;
+  font-size: 24px;
+  font-weight: 500;
+  color: #000;
+  text-align: center;
+  display: block;
+  width: 300px;
+  padding: 10px 20px;
+  margin: 30px auto;
+`;
 
 const Home = () => {
   return (
     <>
-        <Services />
+      <Services />
+      <StyledButton to="/">See my CV</StyledButton>
     </>
   );
 };
